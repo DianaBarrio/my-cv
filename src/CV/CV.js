0db12c97a398ab4ddf07458e0d7d478efbe1ec46
Diana <dianabarrio@hotmail.com>
2022-02-21 19:33:16 +0100

    first commit

---
+++ src/CV/CV.js
@@ -0,0 +1,71 @@
+export const CV = {
+    hero: {
+      name: "Diana",
+      adress: "Barrio Gil",
+      city: "Madrid - España",
+      email: "drrrrrrr@hotmail.com",
+      birthDate: "29/04/1987",
+      phone: "555555555",
+      image: "https://jobtoday-prod.s3.eu-central-1.amazonaws.com/img/ce39f0f5-024e-4262-bdef-23b802a42f83/640x640.jpg",
+      gitHub: "https://github.com/DianaBarrio",
+      
+    },
+    education: [
+      {
+        name: "Curso de Diseño Gráfico y Maquetación",
+        date: "2005",
+        where: "Academia AZPE",
+      },
+      {
+        name: "Curso de FF.PP I en Diseño Gráfico",
+        date: "2006",
+        where: "Colegio Sagrado Corazón de La Salle.",
+      },
+      {
+        name: "Bootcamp de programación",
+        date: "2021",
+        where: "Academia Upgrade-hub",
+      },
+    ],
+    experience: [
+      {
+        name: "Dependienta",
+        date: "05/05/2010-15/09/2012",
+        where: "TopBooks",
+        description:
+          "Caja,atención al cliente, reposición y limpieza de la tienda",
+      },
+      {
+        name: "Diseño",
+        date: "01/01/2008 - 28/02/2012",
+        where: "La Compañia",
+        description:
+          "Editar anuncion para Página Amarillas on-line",
+      },
+      {
+        name: "Diseño",
+        date: "05/05/2015 - 28/09/2021",
+        where: "Plug Brokers",
+        description:
+          "Diseño Gráfico para carteles, tarjetas, imagenes para la web de la empresa y apoyo en diseño web con WordPres",
+      },
+    ],
+    languages: {
+      language: "Inglés",
+      wrlevel: "medio",
+      splevel: "medio",
+    },
+    habilities: [
+      "Paquete Offices",
+      "Photoshop",
+      "Freehand",
+      "Indesaing",
+      "Director",
+      "Illustrator",
+      "Joomla",
+      "Premier",
+      "Dreamweaver",
+      "WordPres",
+    ],
+    
+  };
